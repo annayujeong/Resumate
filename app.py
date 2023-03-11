9d5a536403e0bb37a4f874e9f91d276c7c2e2b55
Anna Yujeong An <yujeong99an@gmail.com>
2023-03-11 12:35:01 -0800

    Drop files from .gitignore

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from course import response
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello!"
+    return response()
 
 @app.route("/homepage")
 def hello_from_home():
