3eb6b1fb0d4274e29bc45f1827332585722c690e
Anna Yujeong An <yujeong99an@gmail.com>
2023-03-11 15:41:51 -0800

    Add test function

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from course import response
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello():
@@ -11,3 +13,8 @@ def hello():
 @app.route("/homepage")
 def hello_from_home():
     return "Hello from the homepage"
+
+@app.route("/temp", methods=['GET'])
+def temp_func(temp):
+    print(temp)
+    return temp
