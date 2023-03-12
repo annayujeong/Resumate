0786d8a808f833e582a81e43d21f195eb0f6c7c4
Anna Yujeong An <yujeong99an@gmail.com>
2023-03-12 11:09:06 -0700

    Code cleanup

--- app.py
+++ app.py
@@ -2,16 +2,10 @@ import ast
 from flask import Flask, request
 from course import response
 from flask_cors import CORS
-
 from resumeRewriter import rewriteResume
-# from resumeRewriter import highlightDifferences
 
 app = Flask(__name__)
 CORS(app)
-
-@app.route("/job_resume/", methods=['POST'])
-def job_resume():
-    return request.data
 
 @app.route("/courses", methods=['POST'])
 def courses():
@@ -20,25 +14,12 @@ def courses():
     data['job_description'] = request.form['job_description']
     return response(data)
 
-@app.route("/homepage")
-def hello_from_home():
-    return "Hello from the homepage"
-
-@app.route("/stuff", methods=['POST'])
-def stuff():
-    return 
-
 @app.route("/resumeResults")
 def resume():
-    #rewriteResume()
-    #return highlightDifferences()
     return rewriteResume()
 
-# POSTing data from the front end
 @app.route("/results", methods=['POST'])
 def postResume():
     data = request.form['resume']
 
     return rewriteResume(data)
-    # return data
-    # return request.form['resume']
--- course.py
+++ course.py
@@ -1,17 +1,12 @@
-import ast
 import openai
-from PyPDF2 import PdfReader
 from dotenv import load_dotenv
 import os
-import constants
-import json
 
 def request(input):
     load_dotenv()
     openai.api_key = os.getenv("CHATGPT_API_KEY")
     model_engine = "text-davinci-003"
 
-    # to_dict = ast.literal_eval(input.decode('utf-8'))
     resume = input['resume']
     job_description = input['job_description']
     prompt = f"This is my resume: {resume}, and this is the description of a job I'm interested in:{job_description}. What are some courses and their URLs, in format of course name&&&course url, separated by***, that I could take to be better suited for the job?"
@@ -20,7 +15,6 @@ def request(input):
         engine=model_engine,
         prompt=prompt,
         max_tokens=1024,
-        # stop=None,
         temperature=0.9,
         top_p=1,
         frequency_penalty=0.0,
